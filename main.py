975f57d394e5f990ee8ebd23a18e461fb888dad4
Jaeyeong Won <devlog.jae13@gmail.com>
2025-08-18 17:54:13 +0900

    ✨ : Gemini AI 모델 추가

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ import io
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
+
+load_dotenv()
 
 # --- Pydantic 모델 정의 ---
-
 # /index API가 받을 요청 Body의 구조
 class IndexRequest(BaseModel):
     travel_list_id: int
@@ -24,8 +29,9 @@ class ChatRequest(BaseModel):
 # --- FastAPI 앱 생성 ---
 app = FastAPI()
 
+# 텍스트를 벡터로 변환하는 모델
 embeddings = HuggingFaceEmbeddings(model_name="jhgan/ko-sroberta-multitask")
-
+llm = ChatGoogleGenerativeAI(embeddings="gemini-pro")
 vector_stores = {}
 
 @app.get("/")
@@ -56,18 +62,34 @@ def index_pdf(request: IndexRequest):
             vector_stores[travel_id] = db
         else:
             vector_stores[travel_id].merge_from(db)
-
-        print(f"PDF for travel_list_id {travel_id} indexed successfully.")
         return {"message": "PDF 처리와 인덱싱이 성공적으로 완료되었습니다."}
 
     except Exception as e:
-        print(f"PDF 처리 실패: {e}")
         return {"message" : f"PDF 처리 실패. Error: {e}"}
 
 
 # --- /chat API 엔드포인트 ---
 @app.post("/chat")
 def chat_with_bot(request: ChatRequest):
-    print(f"Chat 요청 받음: travel_list_id={request.travel_list_id}, question='{request.question}'")
-    # TODO: 여기에 Vector DB에서 관련 내용을 검색하고 LLM에게 답변을 생성하도록 요청하는 로직 추가
-    return {"answer": f"'{request.question}'에 대한 임시 답변입니다."}
+    travel_id = request.travel_list_id
+    question = request.question
+
+    # 해당 여행에 대해 학습된 벡터 저장소가 있는지 확인
+    if travel_id not in vector_stores:
+        return {"answer":"아직 학습된 PDF 문서가 없습니다. 먼저 문서를 업로드해주세요."}
+
+    db = vector_stores[travel_id]
+
+    # Langchain을 사용하여 QA 체인 생성
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=db.as_retriever()
+    )
+
+    # QA 체인을 실행하여 답변 생성
+    try:
+        answer = qa_chain.run(question)
+        return {"answer": answer}
+    except Exception as e:
+        return {"answer": "답변을 생성하는 중 오류가 발생했습니다."}
